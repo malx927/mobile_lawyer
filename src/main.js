da419932d0b51b5c5a601d4ab1e5eeb8f251e0fb
mlx927 <5971158@qq.com>
2020-05-19 17:01:09 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import {Button} from 'vant'
+import { Button, NavBar, Tabbar, TabbarItem} from 'vant';
 
 Vue.config.productionTip = false
-Vue.use(Button)
+
+Vue.use(Button);
+Vue.use(NavBar);
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
 
 new Vue({
   router,
